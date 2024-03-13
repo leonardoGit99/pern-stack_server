ca54e7a746c67151263c7b5ac22d886f99ee72cc
Leonardo Fuentes Claros <51714281+leonardoGit99@users.noreply.github.com>
2024-03-12 22:13:34 -0400

    Add crud endpoints with a message response

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /* Archivo que arranca el servidor */
 const express = require('express');
-const morgan = require('morgan');
+const morgan = require('morgan'); //Morgan es un middleware que se ejecuta antes y durante una solicitud HTTP, registra los request entre otros.
+
+const taskRoutes = require('./routes/tasks.routes'); //importamos el modulo tasksroutes.js 
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(taskRoutes);
 
 const PORT = 4000;
 app.listen(PORT);
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,1 +1,29 @@
-/* End Points */
+/* End Points que el frontend pueda utilizar*/
+
+const { Router } = require('express'); // Extraemos Router de express y lo ejecutamos para que nos permita hacer el routing en nuestra app
+
+const router = Router();
+
+router.get('/tasks', (req, res) => {
+  res.send('retrieving a list of tasks');
+});
+
+router.get('/tasks/10', (req, res) => {
+  res.send('retrieving a single task');
+});
+
+router.post('/tasks', (req, res) => {
+  res.send('creating a task');
+});
+
+router.delete('/tasks', (req, res) => {
+  res.send('deleting a task');
+});
+
+router.put('/tasks', (req, res) => {
+  res.send('updating a task');
+});
+
+
+
+module.exports = router; //Exportamos el modulo (objeto) router que contiene las distintas rutas de la app.
