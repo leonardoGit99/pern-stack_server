39c46571f994e6ce619f2bcd464a79299a5a59ad
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-05-21 23:55:48 -0400

    Update host for data base, supabase instead of elephantsql

--- server/src/config.js
+++ server/src/config.js
@@ -16,11 +16,7 @@ cloudinary.config({
 
 module.exports = {
   db: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    urldatabase: process.env.DATABASE_URL,
   },
   cloudinary
 }
